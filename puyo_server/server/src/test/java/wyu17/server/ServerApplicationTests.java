4d34f61fb05d2a55e5766adb89c25672a3ef549e
Wilson Yu <wilsonyu1709@gmail.com>
2021-02-01 23:59:16 +1000

    add spring boot code

---
+++ puyo_server/server/src/test/java/wyu17/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wyu17.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
